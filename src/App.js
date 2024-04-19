d8bbfa7d7e1d57aa22c07f75ea86c30cd61f6412
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-04-19 20:03:28 +0600

    all files added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singer = { name: "Atif Aslam", job: "singer" };
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>React is going on.</h3>
+          <div className="music">
+            <p>Name: {singer.name} </p>
+            <p>Job: {singer.job} </p>
+          </div>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is introduction of first React App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explore First React
         </a>
       </header>
     </div>
