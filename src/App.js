9c6210614d126b1e87ab2c39855ec35657871429
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-04-20 02:06:24 +0600

    component making

--- src/App.js
+++ src/App.js
@@ -8,29 +8,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>React is going on.</h3>
-          <div className="music">
-            <p>Name: {singer.name} </p>
-            <p>Job: {singer.job} </p>
-          </div>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This is introduction of first React App</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Explore First React
-        </a>
+        <Person></Person>
+        <Friend></Friend>
+
       </header>
     </div>
   );
 }
 
+function Person() {
+  return (
+    <div className='person'>
+      <h1>Hello React</h1>
+      <p>Welcome to the react world!</p>
+    </div>
+  )
+}
+
+function Friend() {
+  return (
+    <div>
+      <h3>Library of Java Script</h3>
+    </div>
+  )
+}
 export default App;
